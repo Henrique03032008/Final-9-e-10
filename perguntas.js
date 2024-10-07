ca21a8d0c042de3582ca0e59cda68c344ba60950
Henrique03032008 <142220627+Henrique03032008@users.noreply.github.com>
2024-10-07 07:35:29 -0300

    Final
    07/10/2024

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Brasil',
+    'O que é Brasil?',
+    'O Brasil é um pais verde e belo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é brasilia'
+)
+
+criaCartao(
+    'Música',
+    'Tipos de música',
+    'Gauchas, rock, sertanejo, etc'
+)
+criaCartao(
+    'Italiano',
+    'Como se diz oi em Italiano?',
+    'Oi em italiano é Ciao'
+)
